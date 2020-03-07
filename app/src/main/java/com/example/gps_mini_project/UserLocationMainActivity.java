d28d4b79565eccb9bcbfae5d938c532199330980
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2020-03-07 18:11:52 +0530

    Current Location Detection Setup done!!!

--- app/src/main/java/com/example/gps_mini_project/UserLocationMainActivity.java
+++ app/src/main/java/com/example/gps_mini_project/UserLocationMainActivity.java
@@ -1,16 +1,31 @@
 package com.example.gps_mini_project;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.internal.GoogleApiAvailabilityCache;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -19,6 +34,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -27,10 +45,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class UserLocationMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
-    private GoogleMap mMap;
-    private AppBarConfiguration mAppBarConfiguration;
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
+public class UserLocationMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, LocationListener {
+     GoogleMap mMap;
+     AppBarConfiguration mAppBarConfiguration;
     FirebaseAuth auth;
+    GoogleApiClient client;
+    LocationRequest request;
+    LatLng latLng;
+
 
 
     @Override
@@ -40,6 +67,7 @@ public class UserLocationMainActivity extends AppCompatActivity implements Navig
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         auth=FirebaseAuth.getInstance();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -53,7 +81,7 @@ public class UserLocationMainActivity extends AppCompatActivity implements Navig
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.map);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
@@ -77,47 +105,40 @@ public class UserLocationMainActivity extends AppCompatActivity implements Navig
 
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        client = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        client.connect();
     }
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-       int id=item.getItemId();
-       if(id==R.id.nav_joinCircle)
-       {
+        int id = item.getItemId();
+        if (id == R.id.nav_joinCircle) {
 
-       }
-        else if(id==R.id.nav_myCircle)
-        {
+        } else if (id == R.id.nav_myCircle) {
+
+        } else if (id == R.id.nav_signout) {
+
+        } else if (id == R.id.nav_shareLocation) {
+
+        } else if (id == R.id.nav_inviteMembers) {
+
+        } else if (id == R.id.nav_signout) {
+            FirebaseUser user = auth.getCurrentUser();
+            if (user != null) {
+                auth.signOut();
+                finish();
+                Intent i = new Intent(UserLocationMainActivity.this, MainActivity.class);
+                startActivity(i);
+            }
 
         }
-       else if(id==R.id.nav_signout)
-       {
-
-       }
-       else if(id==R.id.nav_shareLocation)
-       {
-
-       }
-       else if(id==R.id.nav_inviteMembers)
-       {
-
-       }
-       else if(id==R.id.nav_signout)
-       {
-           FirebaseUser user=auth.getCurrentUser();
-           if(user!=null)
-           {
-               auth.signOut();
-               finish();
-               Intent i=new Intent(UserLocationMainActivity.this,MainActivity.class);
-               startActivity(i);
-           }
-
-       }
 
         return false;
     }
@@ -126,7 +147,55 @@ public class UserLocationMainActivity extends AppCompatActivity implements Navig
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         int id=item.getItemId();
-      //  if(id==)
+        //  if(id==)
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        new LocationRequest();
+        request= LocationRequest.create();
+    request.setInterval(1000);
+    request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+    if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+    {
+        return;
+    }
+
+    LocationServices.FusedLocationApi.requestLocationUpdates(client,request,this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if(location==null)
+        {
+            Toast.makeText(getApplicationContext(),"Could not find Location",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            latLng=new LatLng(location.getLatitude(),location.getLongitude());
+            MarkerOptions options=new MarkerOptions();
+            options.position(latLng);
+            options.title("Current Location");
+            mMap.addMarker(options);
+
+        }
     }
 }
