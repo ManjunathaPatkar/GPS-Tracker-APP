23f30b8db40d604e3d33ae35f316d7a372e1cc4d
Manjunatha Patkar <manjunathapatkar150@gmail.com>
2020-03-16 10:36:04 +0530

    Getting your cicle mebers locstion done!!!!

--- app/src/main/java/com/example/gps_mini_project/MembersAdapter.java
+++ app/src/main/java/com/example/gps_mini_project/MembersAdapter.java
@@ -1,6 +1,8 @@
 package com.example.gps_mini_project;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -54,11 +61,13 @@ public class MembersAdapter extends RecyclerView.Adapter<MembersAdapter.MembersV
     {
 
         TextView t1;
-
+        DatabaseReference reference;
+        String lati,longi;
         Context c;
         ArrayList<CreateUser> namearrayList;
         FirebaseAuth auth;
         FirebaseUser user;
+        CreateUser s;
 
 
         public MembersViewHolder(@NonNull View itemView, Context c,ArrayList<CreateUser> namearrayList) {
@@ -78,10 +87,30 @@ public class MembersAdapter extends RecyclerView.Adapter<MembersAdapter.MembersV
         public void onClick(View v) {
 
             int a = getPosition();
-            CreateUser s=namearrayList.get(a);
+            s=namearrayList.get(a);
+            String userid;
+            userid=user.getUid();
+            reference= FirebaseDatabase.getInstance().getReference().child("users").child(userid);
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                  lati =dataSnapshot.child("lat").getValue(String.class);
+                  longi =dataSnapshot.child("lng").getValue(String.class);
+                    Toast.makeText(c, "    "+lati+"    "+longi, Toast.LENGTH_SHORT).show();
+                    String uri = "http://maps.google.com/maps?saddr=" + lati + "," + longi + "&daddr=" + s.lat + "," + s.lng;
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                    intent.setPackage("com.google.android.apps.maps");
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    c.startActivity(intent);
 
+                }
 
-            Toast.makeText(c, ""+s.name, Toast.LENGTH_SHORT).show();
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
 
         }
     }
